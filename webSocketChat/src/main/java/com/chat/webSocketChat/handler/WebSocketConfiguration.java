b4b5c18010115a7efbbde9ed90709bd7f3322920
mihyeon <mihyeon.kg@gmail.com>
2023-12-18 16:15:05 +0900

    지원하지 않는 브라우저에도 비슷하게 사용하도록 추가

--- webSocketChat/src/main/java/com/chat/webSocketChat/handler/WebSocketConfiguration.java
+++ webSocketChat/src/main/java/com/chat/webSocketChat/handler/WebSocketConfiguration.java
@@ -12,9 +12,11 @@ public class WebSocketConfiguration implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        // * withSockJS(): 지원하지 않는 범위의 브라우저에서도 웹소켓과 비슷하게 사용
         registry
                 .addHandler(signalingSocketHandler(), "/room")
-                .setAllowedOrigins("*");
+                .setAllowedOrigins("*")
+                .withSockJS();
     }
 
     @Bean
