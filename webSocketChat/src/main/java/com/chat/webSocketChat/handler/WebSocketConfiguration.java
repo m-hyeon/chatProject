9088360fb044e6af40c59f06837254a04f11b3d8
mihyeon <alguscool12@gmail.com>
2024-01-08 08:31:08 +0900

    2024.01.08 - sockjs 삭제

--- webSocketChat/src/main/java/com/chat/webSocketChat/handler/WebSocketConfiguration.java
+++ webSocketChat/src/main/java/com/chat/webSocketChat/handler/WebSocketConfiguration.java
@@ -15,8 +15,7 @@ public class WebSocketConfiguration implements WebSocketConfigurer {
         // * withSockJS(): 지원하지 않는 범위의 브라우저에서도 웹소켓과 비슷하게 사용
         registry
                 .addHandler(signalingSocketHandler(), "/room")
-                .setAllowedOrigins("*")
-                .withSockJS();
+                .setAllowedOrigins("*");
     }
 
     @Bean
